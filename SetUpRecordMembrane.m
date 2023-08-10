4bf6a774ea2e7426b490a5853a47657e4db25d23
Mendoza Lab - Huntsman Cancer Institute <89532889+MendozaLabHCI@users.noreply.github.com>
2023-08-10 10:29:12 -0600

    Delete SetUpRecordMembrane.m

--- SetUpRecordMembrane.m
+++
@@ -1,8 +0,0 @@
-function RecordMembrane = SetUpRecordMembrane(TimeVec,Membrane)
-
-    RecordMembrane.Nodes = zeros(size(Membrane.Nodes,1),2,length(TimeVec),'single');
-    RecordMembrane.Segments = Membrane.Segments;
-    RecordMembrane.Springs = Membrane.Springs;
-    RecordMembrane.TimeVec = TimeVec;   
-
-end
