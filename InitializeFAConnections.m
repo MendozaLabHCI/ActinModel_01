9a61c12f49382f3a6236e855b0f59baecbfd5e87
Mendoza Lab - Huntsman Cancer Institute <89532889+MendozaLabHCI@users.noreply.github.com>
2023-08-10 10:26:50 -0600

    Delete InitializeFAConnections.m

--- InitializeFAConnections.m
+++
@@ -1,7 +0,0 @@
-function FAConnections = InitializeFAConnections
-
-     FAConnections.AdhesionIndex = zeros(0,1);
-     FAConnections.FilamentName  = zeros(0,1);
-     FAConnections.MonomerIndex  = zeros(0,1);
-     
-end
